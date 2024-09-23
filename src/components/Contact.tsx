5c747326fd7c08c4101dae7f4ebf4c8c4736ee55
Christophe SAURY <christophesaury23@gmail.com>
2024-09-23 20:44:59 +0200

    created contact section

---
+++ src/components/Contact.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Contact = () => {
+  return (
+    <div className="border-b border-neutral-900 pb-20">
+      <h1>Get in Touch</h1>
+    </div>
+  );
+};
+
+export default Contact;
